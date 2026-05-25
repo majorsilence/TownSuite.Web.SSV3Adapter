52d11d65d7a26795d77763157275768aaf6aecb4
Peter Gill <peter.gill@townsuite.com>
2026-05-25 16:17:16 -0230

    Point version updater to Directory.Build.props
    Replace three per-project RunVerionUpdater calls with a single call targeting Directory.Build.props. This centralizes version number updates in the shared props file instead of updating individual SSV3Adapter csproj files.

--- PrepareVersionNumbers.ps1
+++ PrepareVersionNumbers.ps1
@@ -10,6 +10,4 @@ function RunVerionUpdater($loc, $path){
 	}	
 }
 
-RunVerionUpdater 3 "$cwd/TownSuite.Web.SSV3Adapter/TownSuite.Web.SSV3Adapter.csproj"
-RunVerionUpdater 3 "$cwd/TownSuite.Web.SSV3Adapter.Interfaces/TownSuite.Web.SSV3Adapter.Interfaces.csproj"
-RunVerionUpdater 3 "$cwd/TownSuite.Web.SSV3Adapter.Prometheus/TownSuite.Web.SSV3Adapter.Prometheus.csproj"
+RunVerionUpdater 3 "$cwd/Directory.Build.props"
